75f34c8c3f1104717527b664c14e8546b44b2639
Roscalion <Roscalion@users.noreply.github.com>
2022-03-17 01:46:14 -0600

    SecondLife.js
    Added the SecondLife Manison Shopping in Javascript

---
+++ Second Life Mansion Shopping for Javascript.js
@@ -0,0 +1,151 @@
+
+/* 
+// SECONDLIFE MASNION -- JAVASCRIPT //
+
+TERP LORE: SecondLife is a metaverse where ROSCA and JINX frequented after WAR 83 in Callahan. After using 
+his money from helping JINX win, ROSCA would help use it to help with research for SecondLife Real Estate. 
+
+So ROSCA would be seen coding a plan for them to have mansions in SecondLife after they were able to get back 
+there again. 
+*/ 
+
+
+
+// SECONDLIFE MANSIONS  //
+
+const mansionsSecondLife = {
+    
+    name: "The Takki Mansion",
+    link: "https://marketplace.secondlife.com/p/The-Takki-Mansion/7110478",
+    cost: "$L4,700",
+    description: "The Takki Mansion is an Japanese styled mansion. It is very interseting and high end in design. ",
+
+    name: "villa Barcelona Mansion Part hMesh" ,
+    link:"https://marketplace.secondlife.com/p/Villa-Barcelona-Mansion-Part-Mesh/7023990",
+    cost: "L$3,950",
+    description: "Spanish themed mansion at a good price.",
+
+    name: "primarveraInToscananMansionBag",
+    link: "https://marketplace.secondlife.com/p/01-8f8-primavera-in-Toscana-Mansion-Bag-RARE/19732588",
+    cost: "L$L1,150",
+    description: "Spanish villa.",
+    
+    name: "Asian Mansion II",
+    link: "https://marketplace.secondlife.com/p/Asian-Mansion-II/609905",
+    cost: "L$1,500",
+    description: "Asian themed mansion",
+
+    name: "Villa De Paris Mansion",
+    link: 'https://marketplace.secondlife.com/p/Paris-Mansion/2604136',
+    cost: "L$1,499",
+
+    name: "Villa De Paris Mansion (Unfurnished)",
+    link: "https://marketplace.secondlife.com/p/Vista-Mansion-Unfurnished/13479619",
+    cost: "L$2,899",
+
+
+    name: "Christmas Villa Mansion",
+    link: "https://marketplace.secondlife.com/p/Christmas-Vista-Mansion/13561671",
+    cost: "L$3,499",
+    description: "Makes a good mansion for Christmas parties",
+}
+
+
+// SECOND LIFE YACHTS //
+
+const yachtsSecondLife = {
+    name: "Legend Yacht",
+    link: "https://marketplace.secondlife.com/p/YACHT/12577278",
+    cost: "L$10,000",
+    description: "White yacht, high end value",
+
+    name: "Carmen Yacht",
+    link: "https://marketplace.secondlife.com/p/YACHT/6093056",
+    cost: "L$2,000",
+    description: "White",
+
+    name: "Yacht Yuri",
+    link: "https://marketplace.secondlife.com/p/Yacht-Yuri/10605468",
+    cost: "L$900",
+    description: "Black/White",
+
+    name: "Carmen Yacht",
+    link: "https://marketplace.secondlife.com/p/Yacht-Carmen/6093169",
+    cost: "L$2,000",
+    description: "Pink Yacht",
+
+    name: "Super Yacht Club",
+    link: "https://marketplace.secondlife.com/p/Yacht-Party-V4/8967869",
+    cost: "L$10,000",
+    description: "High end yacht, has a jacuzzi too!",
+
+    name: "Preadator Yacht",
+    link: "https://marketplace.secondlife.com/p/YACHT-PREDATOR/11378550",
+    cost: "L$7,500",
+    description: "Speedboat type yacht. Yellow and black colors look cool too.",
+
+    name: "Princess Yacht",
+    link: "https://marketplace.secondlife.com/p/Yacht-Princess-II/13648719",
+    cost: "L$5,000",
+    description: "High end Yacht that has an additional jet ski",
+
+
+
+}   
+
+
+// SECOND LIFE CASTLE //
+
+const castleSecondLife = {
+
+    castleName: "Skye Gohthic Castle 2020",
+    link: "https://marketplace.secondlife.com/p/NEW-Skye-Gothic-Castle-2020/19440844",
+    cost: "L$4,950",
+    description: "High end gothic castle. Can be used in the long term",
+
+    castName: "Draco Castle",
+    link: "https://marketplace.secondlife.com/p/Draco-Castle238-Li/5026543",
+    cost: "L$1,890",
+    description: "Could go well with our gothic castles",
+
+}
+
+
+
+
+// ISLAND --- Can be used like Rosca's Island// 
+
+const islandSecondLife = {
+    islandName: "V2 Skye Sandy Beach Building Set with Waves: 100% MESH",
+    link: "https://marketplace.secondlife.com/p/V2-Skye-Sandy-Beach-Building-Set-with-Waves-100-MESH/4485072",
+    cost: "L$999",
+    description: "Simple island that can be used to create a SecondLife version of Rosca's island.",
+
+    islandName: "Sakura Tree",
+    link: "https://marketplace.secondlife.com/p/Skye-Sakura-Tree-Four-Season-Cherry-Tree/14272062",
+    cost: "L$699",
+    description: "Would go with our asian themed mansions",
+
+    islandName: "Skye Woodland Path Building Set",
+    link: "https://marketplace.secondlife.com/p/Skye-Woodland-Path-Building-Set/20193223",
+    cost: "L$899",
+    descripion: "Building set",
+
+
+}
+
+
+// SECONDLIFE WARDEN FASHION -- Used for Warden fashion //
+
+var wardenFashion = {
+
+    nameFashion: "Blue P.I Trench Coat ",
+    link: "https://marketplace.secondlife.com/p/FULL-ZED-MESH-MATERIALS-ENABLED-Blue-PI-Leather-Trench-Coat-Available-in-6-Colours/6226178",
+    type: "Blue Trench Coat",
+    cost: "L$298",
+}
+
+
+
+console.log(wardenFashion)
+console.log(islandSecondLife)
